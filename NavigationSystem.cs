79846821d10c6449067c06a4562f51347d801ec5
Поляков Александр <garzilka@mail.ru>
2019-07-07 00:16:56 +0300

    initial commit

---
+++ NavigationSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationSystem : MonoBehaviour
+{
+    
+    public float    minX,   minZ;
+    public float    maxX,   maxZ;
+    private float    X = 5,      Z = 5;
+    private Vector3 Point;
+    private Vector3 Position;
+    private NavMeshAgent agent;
+    
+    public void newPoint()
+    {
+        X = Random.Range(minX, maxX); // позиция
+        Z = Random.Range(minZ, maxZ); // позиция 
+    }
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        newPoint();
+        Point.x = X;
+        Point.z = Z;
+    }
+    float getDistance() //Дистанция от персонажа до точки
+    {
+        Position = agent.transform.position;
+        float dis = Mathf.Sqrt((X - agent.transform.position.x) +
+                               (Z - agent.transform.position.z));
+        return (dis);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(Point);    //Следовать до точки
+        if(getDistance()<0.5)           //Если врадиусе точки
+        {
+            newPoint();                 //Новая точка
+            Debug.Log("newPoint");
+            Point.x = X;
+            Point.z = Z;
+        }
+    }
+}
