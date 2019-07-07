8d6dc1e16e971c1e30183618a6e3c7da8e4f4069
Поляков Александр <garzilka@mail.ru>
2019-07-07 22:26:14 +0300

    initial commit

--- Interaction.cs
+++ Interaction.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class Interaction : MonoBehaviour
 {
+    private NavigationSystem NS; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        NS = GetComponent<NavigationSystem>();
     }
-    void OnCollisionEnter(Collision other){
+    void OnTriggerEnter(Collision other)
+    {
         print(other.gameObject.name);
-
+        Vector3 Pos = other.transform.position;
+        NS.SetPoint(Pos.x, Pos.z);
+        NS.Stop = true;
     }
     // Update is called once per frame
     void Update()
